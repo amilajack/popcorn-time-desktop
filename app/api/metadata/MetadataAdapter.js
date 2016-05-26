387dee6cdf1dfb576df3d6c1b6afc46acf39c9f0
Amila Welihinda <amilajack@gmail.com>
2016-05-26 00:34:30 -0700

    Removed font awesome from deps

--- app/api/metadata/MetadataAdapter.js
+++ app/api/metadata/MetadataAdapter.js
@@ -3,5 +3,42 @@
  */
 
 export default function MetadataAapter() {
-
+  const providers = [
+    require('./TraktMetadataProvider')
+  ];
 }
+
+/**
+ * Get details about a specific movie
+ *
+ * @param string imdbId
+ */
+function getMovie(imdbId) {}
+
+/**
+ * Get list of movies with specific paramaters
+ *
+ * @param number page
+ * @param number limit
+ * @param string genre
+ * @param string sortBy
+ */
+function getMovies(page, limit, genre, sortBy) {}
+
+/**
+ * Get list of movies with specific paramaters
+ *
+ * @param string query
+ * @param number page
+ * @param number limit
+ * @param string genre
+ * @param string sortBy
+ */
+function search(query, page, limit, genre, sortBy) {}
+
+/**
+ * Get list of movies with specific paramaters
+ *
+ * @param string imdbId
+ */
+function similar(imdbId) {}
---
+++ app/api/metadata/TraktMetadataProvider.js
@@ -0,0 +1,7 @@
+export default class TraktMetadataAdapter {
+  
+}
+
+export function search() {
+
+}
