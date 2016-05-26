d66466c065469ec82871bdb9c80f711e0c7933e6
Amila Welihinda <amilajack@gmail.com>
2016-05-26 16:25:49 -0700

    Updated trakt app keys

--- app/api/metadata/TraktMetadataProvider.js
+++ app/api/metadata/TraktMetadataProvider.js
@@ -3,9 +3,9 @@ import Trakt from 'trakt.tv';
 
 export default class TraktMetadataAdapter {
 
-  client_id = 'd395c9152654ea6ef4e0107d203b1f217cdf66ed01b6e047fa51a9e8cb93956f';
+  client_id = '647c69e4ed1ad13393bf6edd9d8f9fb6fe9faf405b44320a6b71ab960b4540a2';
 
-  client_secret = '52b30c468753bbcf60a4138f510b3eb655ad6d21f70b4848aa6641381ca7d003';
+  client_secret = 'f55b0a53c63af683588b47f6de94226b7572a6f83f40bd44c58a7c83fe1f2cb1';
 
   headers = {
     'Content-Type': 'application/json',
