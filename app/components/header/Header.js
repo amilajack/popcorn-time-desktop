615ed15152e0a5eb69333c402ff9756efd7b5b95
Amila Welihinda <amilajack@gmail.com>
2016-05-26 14:12:28 -0700

    Added search bar to header

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -13,6 +13,10 @@ export default class Header extends Component {
               <span className="sr-only">(current)</span>
             </a>
           </div>
+          <form className="form-inline pull-xs-right">
+            <input className="form-control" type="text" placeholder="Search" />
+            <button className="btn btn-success-outline" type="submit">Search</button>
+          </form>
         </nav>
         <nav className="navbar hidden navbar-dark bg-inverse">
           <div className="nav navbar-nav">
