594495128fdbdf8d9b7668b2eb1a3a79de3f4d10
Amila Welihinda <amilajack@gmail.com>
2016-05-28 17:43:52 -0700

    Initial commit for header.js

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react';
+import Butter from '../../api/Butter';
 
 
 export default class Header extends Component {
+
+  constructor() {
+    super();
+
+    this.butter = new Butter();
+
+    this.state = {
+      searchQuery: ''
+    };
+
+    setTimeout(() => {
+      this.search('harry potter');
+    }, 1000);
+  }
+
+  handleSearchChange(event) {
+    this.setState({ searchQuery: event.target.value });
+  }
+
+  async search(query) {
+    if (query.length) {
+      const searchResults = await this.butter.search(query);
+      console.dir(searchResults);
+    }
+  }
 
   render() {
     return (
@@ -14,8 +40,20 @@ export default class Header extends Component {
             </a>
           </div>
           <form className="form-inline pull-xs-right">
-            <input className="form-control" type="text" placeholder="Search" />
-            <button className="btn btn-success-outline" type="submit">Search</button>
+            <input
+              className="form-control"
+              value={this.state.searchQuery}
+              onChange={this.handleSearchChange.bind(this)}
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              className="btn btn-success-outline"
+              onClick={this.search.bind(this, this.state.searchQuery)}
+              type="button"
+            >
+              Search
+            </button>
           </form>
         </nav>
         <nav className="navbar hidden navbar-dark bg-inverse">
