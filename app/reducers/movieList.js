23500c24230501b8a345bfb0aac5b0191fa30c27
Amila Welihinda <amilajack@gmail.com>
2016-06-06 13:17:46 -0700

    Added shileds to readme

--- app/reducers/movieList.js
+++ app/reducers/movieList.js
@@ -1,9 +1,9 @@
-import { PAGINATE_MOVIES } from '../actions/movieListAction';
+// import { PAGINATE_MOVIES } from '../actions/movieListAction';
 
 export default function movieList(state = [], action, movies) {
   switch (action.type) {
-    case PAGINATE_MOVIES:
-      return state.concat([movies]);
+    case 'ADD_MOVIES':
+      return [...state, ...movies];
     default:
       return state;
   }
