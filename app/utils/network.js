4421e3d6ccac1cd4dba2d21158d3cf8471f9ffa1
Zakaria Ridouh <zridouh@live.com>
2016-07-27 17:27:34 -0700

    last lint commit

--- app/utils/network.js
+++ app/utils/network.js
@@ -7,11 +7,12 @@ const test = speedTest({
 });
 
 export default function getSpeed() {
-  test.on ('data', (data) => {
+  test.on('data', (data) => {
     console.log('Download Speed:  ${data.speeds.download} Mb/s');
     console.log('Upload Speed: ${data.speeds.upload} Mb/s');
+    return data; // the whole object for now
   });
-  test.on ('error', (err) => {
+  test.on('error', (err) => {
     console.log('Speed test error:');
     return err;
   });
