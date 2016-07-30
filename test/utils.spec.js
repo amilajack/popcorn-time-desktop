437f7a323b34dc85570bdf2932a7fccda966a4b9
Amila Welihinda <amilajack@gmail.com>
2016-07-30 15:55:41 -0700

    Skip 'bench-api' and 'test-api' tests on macOS, refactored Network module

--- test/utils.spec.js
+++ test/utils.spec.js
@@ -8,19 +8,27 @@ import {
 describe('Network', () => {
   describe('download', () => {
     it('should resolve to a download speed', async done => {
-      const downloadSpeed = getDownloadSpeed();
-      expect(downloadSpeed).to.be.a('promise');
-      expect(await downloadSpeed).to.be.a('number');
-      done();
+      try {
+        const downloadSpeed = getDownloadSpeed();
+        expect(downloadSpeed).to.be.a('promise');
+        expect(await downloadSpeed).to.be.a('number');
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 
   describe('upload ->', () => {
     it('should resolve to an upload speed', async done => {
-      const uploadSpeed = getUploadSpeed();
-      expect(uploadSpeed).to.be.a('promise');
-      expect(await uploadSpeed).to.be.a('number');
-      done();
+      try {
+        const uploadSpeed = getUploadSpeed();
+        expect(uploadSpeed).to.be.a('promise');
+        expect(await uploadSpeed).to.be.a('number');
+        done();
+      } catch (error) {
+        done(error);
+      }
     });
   });
 });
