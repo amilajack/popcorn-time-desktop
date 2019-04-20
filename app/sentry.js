7984b76d5e84a3425340a1e908af5e836de5780d
Amila Welihinda <amilajack@gmail.com>
2019-04-20 11:55:40 -0700

    fix importing sentry

--- app/sentry.js
+++
@@ -1,7 +0,0 @@
-const { init } = require('@sentry/electron');
-
-if (process.env.NODE_ENV === 'production') {
-  init({
-    dsn: 'https://b0d05cee653942148a43b8163bbc6cee@sentry.io/1277263'
-  });
-}
